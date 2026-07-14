1c9e9cf6be4f1b967307573ac2ec2c68eba9136e
bvasquezkeysije <bvasquezkeysije@gmail.com>
2026-07-14 03:49:17 -0500

    fix(diseno): always show all action buttons, disable when not applicable
    The 'Editar archivos' button was hidden via @if when there were no files,
    causing inconsistent button order across rows. Now all buttons are always
    shown but disabled with reduced opacity when not applicable.

--- Sistema-ArteyMetal/resources/views/diseno/index.blade.php
+++ Sistema-ArteyMetal/resources/views/diseno/index.blade.php
@@ -134,11 +134,9 @@
                                         <button type="button" @@click="$dispatch('open-subir-{{ $pedido->id }}')" class="btn-icon-sm bg-amber-600 hover:bg-amber-700" title="Subir diseno">
                                             <img src="{{ asset('icons/Subir-Blanco.png') }}" alt="Subir diseno" class="h-4 w-4 object-contain pointer-events-none">
                                         </button>
-                                        @if($totalArchivos > 0)
-                                        <button type="button" @@click="$dispatch('open-editar-{{ $pedido->id }}')" class="btn-icon-sm bg-purple-600 hover:bg-purple-700" title="Editar archivos">
+                                        <button type="button" @@click="$dispatch('open-editar-{{ $pedido->id }}')" class="btn-icon-sm bg-purple-600 hover:bg-purple-700" title="Editar archivos" {{ $totalArchivos === 0 ? 'disabled style="opacity:0.35;cursor:not-allowed"' : '' }}>
                                             <img src="{{ asset('icons/editar.ico') }}" alt="Editar archivos" class="h-4 w-4 object-contain pointer-events-none">
                                         </button>
-                                        @endif
                                         <button type="button" @@click="$dispatch('open-notificar-{{ $pedido->id }}')" class="btn-icon-sm bg-amber-500 hover:bg-amber-600" title="Notificar al vendedor">
                                             <svg class="h-4 w-4 pointer-events-none" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"/></svg>
                                         </button>
